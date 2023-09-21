84e40d28688636b2e087a42025053e4ae8a79299
elisaul batista <elisaulbatista@pursuit.org>
2023-09-21 09:45:25 -0400

    added  components and routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import LanguageSelection from './components/LanguageSelection'
+import ProgressTracking from './components/ProgressTracking'
+import QuizzesPage from './components/QuizzesPage'
+import SpeakingPractice from './components/SpeakingPractice'
+import VocabularyBuilder from './components/VocabularyBuilder'
 import './App.css'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div>
+    <Router>
+      <Route path="/" element={<LanguageSelection/>}/>
+      <Route path="./vocabulary" element={<VocabularyBuilder/>}/>
+      <Route path='./quizzes' element={<QuizzesPage/>}/>
+      <Route path='./speaking' element={<SpeakingPractice/>}/>
+      <Route path='./progess' element={<ProgressTracking/>}/>
+    </Router>
+   </div>
   )
+
 }
 
 export default App
